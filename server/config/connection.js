19a3980e4ac4fdccaa085d617a1583e7afc0883a
Georgia <ghobs987@gmail.com>
2021-11-16 20:00:30 +0000

    issue 18 sign up form componet added

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/programming-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/mindGjm',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const path = require("path");
 const db = require("./config/connection");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
