aa2194a3f99e23a23e92a03e578d68b4f50b74ab
jjcourtney <jcourtneywork@gmail.com>
2021-11-20 12:56:01 +0000

    Refactored flow for quiz search

---
+++ client/src/utils/valuesForQuizForm.js
@@ -0,0 +1,123 @@
+export const categoryOptions = [
+    {
+        "value": 9,
+        "label": "General Knowledge"
+    },
+    {
+        "value": 10,
+        "label": "Entertainment: Books"
+    },
+    {
+        "value": 11,
+        "label": "Entertainment: Film"
+    },
+    {
+        "value": 12,
+        "label": "Entertainment: Music"
+    },
+    {
+        "value": 13,
+        "label": "Entertainment: Musicals & Theatres"
+    },
+    {
+        "value": 14,
+        "label": "Entertainment: Television"
+    },
+    {
+        "value": 15,
+        "label": "Entertainment: Video Games"
+    },
+    {
+        "value": 16,
+        "label": "Entertainment: Board Games"
+    },
+    {
+        "value": 17,
+        "label": "Science & Nature"
+    },
+    {
+        "value": 18,
+        "label": "Science: Computers"
+    },
+    {
+        "value": 19,
+        "label": "Science: Mathematics"
+    },
+    {
+        "value": 20,
+        "label": "Mythology"
+    },
+    {
+        "value": 21,
+        "label": "Sports"
+    },
+    {
+        "value": 22,
+        "label": "Geography"
+    },
+    {
+        "value": 23,
+        "label": "History"
+    },
+    {
+        "value": 24,
+        "label": "Politics"
+    },
+    {
+        "value": 25,
+        "label": "Art"
+    },
+    {
+        "value": 26,
+        "label": "Celebrities"
+    },
+    {
+        "value": 27,
+        "label": "Animals"
+    },
+    {
+        "value": 28,
+        "label": "Vehicles"
+    },
+    {
+        "value": 29,
+        "label": "Entertainment: Comics"
+    },
+    {
+        "value": 30,
+        "label": "Science: Gadgets"
+    },
+    {
+        "value": 31,
+        "label": "Entertainment: Japanese Anime & Manga"
+    },
+    {
+        "value": 32,
+        "label": "Entertainment: Cartoon & Animations"
+    }
+];
+
+export const typeOptions = [
+    {
+        "value": 'multiple',
+        "label": "Multiple Choice"
+    },
+    {
+        "value": "boolean",
+        "label": "True / False"
+    }
+];
+
+export const difficultyOptions = [
+    {
+        "value": "easy",
+        "label": "Easy"
+    }, {
+        "value": "medium",
+        "label": "Medium"
+    }, {
+        "value": "hard",
+        "label": "Hard"
+    }
+
+];
