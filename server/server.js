faf41b1178790beab62db03080f4731e28ffa7cc
jjcourtney <jcourtneywork@gmail.com>
2021-11-23 23:56:28 +0000

    change port to 5000

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const db = require("./config/connection");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
