74ff321b43625442f9e16654d3888d5d121bfe1a
jjcourtney <jcourtneywork@gmail.com>
2021-11-23 23:56:13 +0000

    added state management for questions and score

---
+++ client/src/components/Quiz.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Button, ListGroup, ProgressBar } from "react-bootstrap";
+const quizQuestions = [
+    {
+        category: "Entertainment: Video Games",
+        type: "multiple",
+        difficulty: "medium",
+        question: "Which company did Bethesda purchase the Fallout Series from?",
+        correct_answer: "Interplay Entertainment ",
+        incorrect_answers: [
+            "Capcom",
+            "Blizzard Entertainment",
+            "Nintendo"
+        ]
+    },
+    {
+        category: "History",
+        type: "multiple",
+        difficulty: "hard",
+        question: "What was the code name for the Allied invasion of Southern France on August 15th, 1944?",
+        correct_answer: "Operation Dragoon",
+        incorrect_answers: [
+            "Operation Overlord",
+            "Operation Market Garden",
+            "Operation Torch"
+        ]
+    },
+    {
+        category: "General Knowledge",
+        type: "multiple",
+        difficulty: "easy",
+        question: "How tall is the Burj Khalifa?",
+        correct_answer: "2,722 ft",
+        incorrect_answers: [
+            "2,717 ft",
+            "2,546 ft",
+            "3,024 ft"
+        ]
+    }
+]
+
+
+
+const Quiz = (props) => {
+    const [questionNumber, setquestionNumber] = useState(0)
+    const [activeQuestion, setActiveQuestion] = useState(0)
+    const [correctAnswers, setCorrectAnswers] = useState(0)
+
+
+    const handleClick = (event) => {
+        setActiveQuestion(event.target.id)
+        console.log(event.target.value)
+    }
+    const handleSubmit = (event) => {
+
+        const answer = parseInt(activeQuestion.replace(/[^0-9]/g, ''));
+        if (answer) {
+            setCorrectAnswers(correctAnswers + 1)
+        }
+        if (questionNumber < quizQuestions.length) {
+            setquestionNumber(questionNumber + 1)
+        }
+
+    }
+
+
+    return (
+        <>
+            <h1>Correct answers: {correctAnswers}</h1>
+            <h2>Question</h2>
+            <p>{quizQuestions[questionNumber].question}</p>
+
+            <ListGroup as="ul">
+
+                {quizQuestions[questionNumber].incorrect_answers.map((answer, index) => `answer-${index}` === activeQuestion ?
+                    <ListGroup.Item key={index} action onClick={handleClick} active id={`answer-${index}`}>{answer}</ListGroup.Item >
+                    :
+                    <ListGroup.Item key={index} action onClick={handleClick} id={`answer-${index}`}>{answer}</ListGroup.Item>)
+                }
+
+            </ListGroup>
+            <br />
+            <hr />
+            <div className="d-grid gap-2">
+                <Button variant="primary center" size="lg" onClick={handleSubmit}>
+                    Submit
+                </Button>
+            </div>
+
+            <br />
+            <ProgressBar now={((questionNumber) * 100) / quizQuestions.length} />
+        </>
+    );
+};
+
+export default Quiz;
