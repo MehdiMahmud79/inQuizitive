9e71774e8585c1ab1856a0181039bdd9abfec366
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-24 21:18:35 +0000

    mutation and queries added to the server/client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-
-
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import AddQuiz from "./pages/AddQuiz";
-
 
 function App() {
   return (
