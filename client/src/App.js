6d157e53eb514288e482ec2f1ab9edf3bf753ab8
jjcourtney <jcourtneywork@gmail.com>
2021-11-25 20:54:48 +0000

    merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,12 @@ import Quiz from './components/Quiz';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <ApolloProvider client={client}>
-      <Router>
-        <Navbar />
-        <Quiz />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/addquiz" element={<AddQuiz />} />
-=======
     <Router>
       <Navbar />
-
+      <Quiz />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/addquiz" element={<AddQuiz />} />
->>>>>>> origin/main
 
         {/* <Route exact path="/quiz" component={CreateQuiz} /> */}
         <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
