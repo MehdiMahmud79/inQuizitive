a8bb75f410672fd60fc73262dc977de47c8d1c1b
jjcourtney <jcourtneywork@gmail.com>
2021-11-28 19:46:06 +0000

    added default

--- server/models/scoreSchema.js
+++ server/models/scoreSchema.js
@@ -3,11 +3,13 @@ const { Schema } = require("mongoose");
 const scoreSchema = new Schema({
   user_id: {
     type: String,
-    required: true
+    required: true,
+    default: ''
   },
   score: {
     type: String,
-    required: true
+    required: true,
+    default: ''
   },
 });
 
