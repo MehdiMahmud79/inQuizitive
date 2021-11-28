7e263a22b2ffd3f5725fe6d5df0d72e30fef0587
jjcourtney <jcourtneywork@gmail.com>
2021-11-28 20:04:11 +0000

    bugfix - quiz scores

--- client/src/components/Quiz/QuizLogic.js
+++ client/src/components/Quiz/QuizLogic.js
@@ -39,6 +39,7 @@ const QuizLogic = ({ quizData, quizId }) => {
   const [questionNumber, setquestionNumber] = useState(0);
   const [activeQuestion, setActiveQuestion] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [answers, setAnswers] = useState([])
 
   const [timeLeft, setTimeLeft] = useState(
     TIME_PER_QUESTION * quizQuestions.length
@@ -48,23 +49,18 @@ const QuizLogic = ({ quizData, quizId }) => {
   const [score, setScore] = useState(0);
 
   function shuffle1(arr) {
-    return Array(arr.length)
+    setAnswers(Array(arr.length)
       .fill(null)
       .map((_, i) => [Math.random(), i])
       .sort(([a], [b]) => a - b)
-      .map(([, i]) => arr[i]);
+      .map(([, i]) => arr[i]))
   }
 
-  let answerArray = [];
-  var arr1 = [...quizQuestions[questionNumber].incorrect_answers];
-  var arr2 = quizQuestions[questionNumber].correct_answer;
-  let answerArray1 = [...arr1, arr2];
 
-  answerArray = answerArray1;
 
   useEffect(() => {
-    answerArray = shuffle1(answerArray1);
-  }, [questionNumber, answerArray]);
+    shuffle1([quizQuestions[questionNumber].correct_answer, ...quizQuestions[questionNumber].incorrect_answers]);
+  }, [questionNumber]);
   const timer = setTimeout(() => {
     if (timeLeft <= 0) {
       clearTimeout(timer);
@@ -83,7 +79,7 @@ const QuizLogic = ({ quizData, quizId }) => {
     if (activeQuestion !== 0) {
       const answerIndex = parseInt(activeQuestion.replace(/[^0-9]/g, ""));
       if (
-        answerArray[answerIndex] ===
+        answers[answerIndex] ===
         quizQuestions[questionNumber].correct_answer
       ) {
         setCorrectAnswers(correctAnswers + 1);
@@ -97,10 +93,14 @@ const QuizLogic = ({ quizData, quizId }) => {
           (correctAnswers / totalQuestions) *
           (timer / totalQuestions) *
           TIME_PER_QUESTION;
+
         setScore(Math.round(totalScore));
-        const scoreString = score.toString();
-        console.log(scoreString);
-        const quizScore = { score: scoreString, user_id: userId };
+
+        const quizScore = {
+          score: `${Math.round(totalScore)}`,
+          user_id: userId
+        };
+
         const mydata = await addScore({
           variables: { id: quizId, score: quizScore },
         });
@@ -116,7 +116,7 @@ const QuizLogic = ({ quizData, quizId }) => {
       <QuizResult
         correctAnswers={correctAnswers}
         quizLength={quizQuestions.length}
-        Result={score}
+        result={score}
         quiz_id={quizId}
         user_id={userId}
         user_name={user_name}
@@ -169,7 +169,7 @@ const QuizLogic = ({ quizData, quizId }) => {
         </p>
         <br />
         <ListGroup as="ul" className=" text-2xl">
-          {answerArray.map((answer, index) =>
+          {answers.map((answer, index) =>
             `answer-${index}` === activeQuestion ? (
               <ListGroup.Item
                 className="list-group-item-primary rounded-b-2xl"
