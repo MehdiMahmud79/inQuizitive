03f0ebec2dd098b0cdceeb3436f0e37d28d40f7c
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-28 16:30:47 +0000

    change public folder

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "public")));
 }
 
 db.once("open", () => {
