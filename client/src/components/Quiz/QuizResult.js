6a21bbbdc151f6f2db19cdbd7e00b60ad1deddf3
jjcourtney <jcourtneywork@gmail.com>
2021-11-28 19:45:06 +0000

    naming issue fixed

--- client/src/components/Quiz/QuizResult.js
+++ client/src/components/Quiz/QuizResult.js
@@ -1,28 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { useMutation, useQuery } from "@apollo/client";
-import { AddScoreToQuizMutation } from "../../utils/queries";
+import React from "react";
 const QuizResult = ({
   correctAnswers,
   quizLength,
-  Result,
-  quiz_id,
-  user_id,
-  user_name,
+  result
 }) => {
 
-   return (
-     <div className="container bg-purple-100 rounded-xl shadow-md p-3">
-       <h1 className="text-center m-3  "> Quiz Result</h1>
-       <hr />
-       <h2 className=" p-1  ">
-         <i className="fas fa-check-square text-green-500"></i>{" "}
-         <span className="text-purple-900 ">{correctAnswers} </span>
-         OUT OF <span className="text-purple-900 text-3xl">{quizLength} </span>
-       </h2>
-       <h2> your Score: {Result} </h2>
-       <hr />
-     </div>
-   );
+  return (
+    <div className="container bg-purple-100 rounded-xl shadow-md p-3">
+      <h1 className="text-center m-3  "> Quiz Result</h1>
+      <hr />
+      <h2 className=" p-1  ">
+        <i className="fas fa-check-square text-green-500"></i>{" "}
+        <span className="text-purple-900 ">{correctAnswers} </span>
+        OUT OF <span className="text-purple-900 text-3xl">{quizLength} </span>
+      </h2>
+      <h2> your Score: {result} </h2>
+      <hr />
+    </div>
+  );
 };
 
 export default QuizResult;
