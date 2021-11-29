f31962f8723c6e4314953181c6a1d23b236c5474
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-11-29 17:34:49 +0000

    final version

---
+++ models/scoreSchema.js
@@ -0,0 +1,21 @@
+const { Schema } = require("mongoose");
+
+const scoreSchema = new Schema({
+  user_id: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  userName: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  score: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+
+module.exports = scoreSchema;
